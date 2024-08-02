2c54c60a5abf3f8b4358571cb0f94f9fd31503a4
selafaurie <selafaurie@gmail.com>
2024-08-02 15:37:36 +0200

    update notebook

--- config.py
+++ config.py
@@ -1,41 +1,40 @@
-
-QUOTE_API = 'https://on-ramp.api.cx.metamask.io'
-PROVIDERS = "/providers"
-QUOTE_ENDPOINT = "/providers/all/quote"
-RANKINGS = "/providers/all/rankings"
-
-
-REGIONS_API = 'https://on-ramp-content.api.cx.metamask.io/regions/'
-
-
-CRYPTOCURRENCIES = {
-    " Ethereum Mainnet": "/currencies/crypto/1/0x0000000000000000000000000000000000000000"
-    , "USDT (Ethereum)": "/currencies/crypto/1/0xdac17f958d2ee523a2206206994597c13d831ec7"
-    , "USDT (BNB Chain)": "/currencies/crypto/56/0x55d398326f99059ff775485246999027b3197955"
+############## REGION - Get Payment
+REGION_PARAMS = {
+    "sdk":"1.28.3"
 }
-
-
-PAYMENT_METHODS = {
-    "Debit": "/payments/debit-credit-card",
-    "SEPA": "/payments/sepa-bank-transfer",
-    "Khipu": "/payments/khipu",
-    "PSE": "/payments/pse",
-    "Faster Payments": "/payments/gbp-bank-transfer",
-    "Instant Bank Transfer": "payments/instant-bank-transfer"
-}
-
-
-COUNTRIES_TO_SCRAPE = {
-    "US": {
-        "region": "/regions/us-fl"
-        , "fiat": "/currencies/fiat/usd"
+REGIONS_API = 'https://on-ramp-content.api.cx.metamask.io/regions'
+REGIONS = [
+    {
+        "region":"de"
+        , "country":"DE"
+        , "fiat_currency": "/currencies/fiat/eur"
+    },
+    {
+        "region":"us-fl"
+        , "country":"US"
+        , "fiat_currency": "/currencies/fiat/usd"
+    },
+    {
+        "region":"gb"
+        , "country":"UK"
+        , "fiat_currency": "/currencies/fiat/gb"
     }
-    , "UK": {
-        "region": "/regions/gb"
-        , "fiat": "/currencies/fiat/gbp"
-    }
-    , "DE": {
-        "region": "/regions/de"
-        , "fiat": "/currencies/fiat/eur"
-    }
-}
+]
+
+############## QUOTE
+QUOTE_API = 'https://on-ramp.api.cx.metamask.io/providers/all/quote'
+CRYPTOCURRENCIES = [
+    {
+        "name": "Ethereum Mainnet"
+        , "id": "/currencies/crypto/1/0x0000000000000000000000000000000000000000"
+    },
+    {
+        "name": "USDT (Ethereum)"
+        , "id":  "/currencies/crypto/1/0xdac17f958d2ee523a2206206994597c13d831ec7"
+    },
+    {
+        "name": "USDT (BNB Chain)"
+        , "id":  "/currencies/crypto/56/0x55d398326f99059ff775485246999027b3197955"
+    },
+    
+]
